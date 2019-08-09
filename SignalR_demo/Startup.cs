b5e7f06a61e2eab6188a2f8d8ef8ad4dcde46e97
Sahil Maiyani <sahilmaiyani@gmail.com>
2019-08-09 11:17:46 +0530

    init: check credential for commit -by sahil

--- SignalR_demo/Startup.cs
+++ SignalR_demo/Startup.cs
@@ -27,7 +27,7 @@ namespace SignalR_demo
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World! SignalR");
             });
         }
     }
