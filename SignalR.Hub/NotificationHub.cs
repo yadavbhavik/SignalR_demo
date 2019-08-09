af9a5af41a241076921a693be338338fef2ef64b
bhavik yadav <yadavbhavi01@gmail.com>
2019-08-09 18:22:01 +0530

    Signalr configuration in hub by bhavik yadav

--- SignalR.Hub/NotificationHub.cs
+++ SignalR.Hub/NotificationHub.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using Microsoft.AspNetCore.SignalR;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SignalR.Hub
 {
-    public class NotificationHub :Hub
+    public class NotificationHub : Microsoft.AspNetCore.SignalR.Hub
     {
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, Context.User.Identity.Name);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, Context.User.Identity.Name);
+            await base.OnDisconnectedAsync(ex);
+        }
 
     }
+
 }
--- SignalR.Hub/Startup.cs
+++ SignalR.Hub/Startup.cs
@@ -15,6 +15,7 @@ namespace SignalR.Hub
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +25,12 @@ namespace SignalR.Hub
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSignalR(routes =>
+            {
+                //routes.MapHub<NotificationsHub>("/notificationhub", options =>
+                //    options.Transports = Microsoft.AspNetCore.Http.Connections.HttpTransports.All);
+            });
 
             app.Run(async (context) =>
             {
