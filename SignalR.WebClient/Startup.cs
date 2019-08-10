dd39fc9875065aef80b33222d5218e481c88095c
Sahil Maiyani <sahilmaiyani@gmail.com>
2019-08-10 12:45:43 +0530

    fix: SignalRHubUrl return null string -By Sahil

--- SignalR.WebClient/Startup.cs
+++ SignalR.WebClient/Startup.cs
@@ -24,7 +24,7 @@ namespace SignalR.WebClient
             services.AddMvc();
 
             // store configuration into AppSettings class -Sahil Maiyani 09-08-2019
-            services.Configure<AppSettings>(Configuration.GetSection("ApplicationSettings"));
+            services.Configure<AppSettings>(Configuration);
             services.AddOptions();
         }
 
