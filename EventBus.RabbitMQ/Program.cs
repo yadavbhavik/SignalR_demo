1a4597bc9c1a39894eee7d8dc71ab2e5d97eefbe
Sahil Maiyani <sahilmaiyani@gmail.com>
2019-08-10 16:22:03 +0530

    init: rabbitmq event bus -By Sahil

---
+++ EventBus.RabbitMQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventBus.RabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
