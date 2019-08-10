c3ad96af06ebbdc6861ce057dee7a3ace8d1c167
Sahil Maiyani <sahilmaiyani@gmail.com>
2019-08-10 18:10:42 +0530

    wip: rabbitmq, connection and create model done

---
+++ EventBusRabbitMQ/IRabbitMQPersistentConnection.cs
@@ -0,0 +1,16 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventBusRabbitMQ
+{
+    public interface IRabbitMQPersistentConnection
+    {
+        bool TryConnect();
+
+        IModel CreateModel();
+
+        bool IsConnected { get; }
+    }
+}
