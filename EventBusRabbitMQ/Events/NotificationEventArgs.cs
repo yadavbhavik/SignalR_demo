c0f8aa510ba992cfecf0a397b0bb3cf17ecd384a
Sahil Maiyani <sahilmaiyani@gmail.com>
2019-08-12 16:47:29 +0530

    add mediatR -By Sahil

---
+++ EventBusRabbitMQ/Events/NotificationEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventBusRabbitMQ.Events
+{
+    public class NotificationEventArgs : System.EventArgs
+    {
+        public NotificationEventArgs(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; }
+    }
+}
