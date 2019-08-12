c99b2deee02a37d8194b5812a61dc6649ba7a54c
Sahil Maiyani <sahilmaiyani@gmail.com>
2019-08-12 11:16:15 +0530

    wip: rabbitmq consumer channel -By Sahil

---
+++ EventBusRabbitMQ/RabbitMQOperation.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventBusRabbitMQ
+{
+    class RabbitMQOperation
+    {
+        private readonly IRabbitMQPersistentConnection persistentConnection;
+        private readonly string queueName;
+        private IModel consumerChannel;
+
+        public RabbitMQOperation(IRabbitMQPersistentConnection persistentConnection, string queueName = null)
+        {
+            this.persistentConnection = persistentConnection;
+            this.queueName = queueName;
+            this.consumerChannel = CreateConsumerChannel();
+        }
+
+        private IModel CreateConsumerChannel()
+        {
+            //check of rabbitmq connection and connect if connection is not exist -Sahil 12-08-2019
+            if (!persistentConnection.IsConnected)
+            {
+                persistentConnection.TryConnect();
+            }
+
+            
+            var channel = persistentConnection.CreateModel();
+            return channel;
+        }
+    }
+}
