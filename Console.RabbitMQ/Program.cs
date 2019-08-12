cbff2817ddb8a20acc75c9f1be82eb1f42b1f5a8
bhavik yadav <yadavbhavi01@gmail.com>
2019-08-12 10:40:29 +0530

    resolve confilct

---
+++ Console.RabbitMQ/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Console.RabbitMQ
+{
+    class Program
+    {
+        private static ConnectionFactory _factory;
+        private static IConnection _connection;
+        private static QueueingBasicConsumer _consumer;
+
+        private const string ExchangeName = "client-update";
+        static void Main(string[] args)
+        {
+            _factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
+            using (_connection = _factory.CreateConnection())
+            {
+                using (var channel = _connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "client-update", type: "direct", true);
+
+                    // var queueName = channel.QueueDeclare().QueueName;
+                    channel.QueueBind(queue: "signalr-global",
+                                      exchange: "client-update",
+                                      routingKey: "notification");
+
+                    System.Console.WriteLine(" [*] Waiting for logs.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        System.Console.WriteLine(" [x] {0}", message);
+                    };
+                    channel.BasicConsume(queue: "signalr-global",
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                }
+            }
+
+        }
+
+    }
+}
