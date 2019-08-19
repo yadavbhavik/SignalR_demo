3c04782cd02c523b88ad758c0ddb5325b34fd6f9
Sahil Maiyani <sahilmaiyani@gmail.com>
2019-08-19 17:14:16 +0530

    init setup testing methods for InMemorySubscriptionManagerTest -By Sahil

---
+++ Tests/SignalR_demo.UnitTests/EventBusRabbitMQ/InMemorySubscriptionManagerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SignalR_demo.UnitTests.EventBusRabbitMQ
+{
+   public class InMemorySubscriptionManagerTest
+    {
+        public InMemorySubscriptionManagerTest() { }
+
+        [Fact]
+        public void GetEventKey_Valid_Return_DataTypeTest()
+        {
+
+        }
+    }
+}
