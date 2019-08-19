300bf15670e90054a48b5f9fcdb0c9f6d97584c2
Sahil Maiyani <sahilmaiyani@gmail.com>
2019-08-19 17:36:35 +0530

    implement unit test for GetEventKey method -By Sahil

--- Tests/SignalR_demo.UnitTests/EventBusRabbitMQ/InMemorySubscriptionManagerTest.cs
+++ Tests/SignalR_demo.UnitTests/EventBusRabbitMQ/InMemorySubscriptionManagerTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EventBusRabbitMQ.Subscription;
+using SignalR.Hub.Events;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -7,12 +9,32 @@ namespace SignalR_demo.UnitTests.EventBusRabbitMQ
 {
    public class InMemorySubscriptionManagerTest
     {
-        public InMemorySubscriptionManagerTest() { }
+        private readonly InMemorySubscriptionsManager inMemorySubscriptionsManager;
+        public InMemorySubscriptionManagerTest() {
+            inMemorySubscriptionsManager = new InMemorySubscriptionsManager();
+        }
 
         [Fact]
-        public void GetEventKey_Valid_Return_DataTypeTest()
+        public void GetEventKey_Valid_Return_Check()
         {
+            //Arrange 
+            var typeName = typeof(StockPriceChangedEvent).Name;
 
+            //Act
+            var returnKey = inMemorySubscriptionsManager.GetEventKey<StockPriceChangedEvent>();
+
+            //Assert
+            Assert.Same(typeName, returnKey);
+        }
+
+        [Fact]
+        public void GetEventKey_NotValidAndNull_Return_Check()
+        {
+            //Arrange-Act
+            var returnKey = inMemorySubscriptionsManager.GetEventKey<StockPriceChangedEvent>();
+
+            //Assert
+            Assert.NotNull(returnKey);
         }
     }
 }
