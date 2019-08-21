94a9d8729ce7e466b4acf4d71d31e32f42b9f2bc
Sahil Maiyani <sahilmaiyani@gmail.com>
2019-08-21 18:39:18 +0530

    event and handler for espay project  global signalR notification -By Sahil

---
+++ SignalR.Hub/Events/espay-layout-beta/BookDataEventNotificationHandler.cs
@@ -0,0 +1,28 @@
+﻿using EventBusRabbitMQ.Events;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Hub.Events.espay_layout_beta
+{
+    public class BookDataEventNotificationHandler : IIntegrationEventHandler<BookDataNotificationEvent>
+    {
+        private readonly IHubContext<NotificationHub> hubContext;
+
+        public BookDataEventNotificationHandler(IHubContext<NotificationHub> hubContext)
+        {
+            this.hubContext = hubContext;
+        }
+        public Task Handle(BookDataNotificationEvent @event)
+        {
+            //TODO: send received data -Sahil 21-08-2019
+            //      To: the signalR groups connection client 
+            //      From: event by RabbitMQ connsumer 
+            //method e.x from espay-layout-beta project 
+            //hubContext.Clients.Group(@event.Type + @event.Pair).SendAsync();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ SignalR.Hub/Events/espay-layout-beta/BookDataNotificationEvent.cs
@@ -0,0 +1,22 @@
+﻿using EventBusRabbitMQ.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Hub.Events.espay_layout_beta
+{
+    public class BookDataNotificationEvent: IntegrationEvent
+    {
+        public string Type { get; }
+        public string Pair { get; }
+        public string Data { get; }
+
+        public BookDataNotificationEvent(string type, string pair, string data)
+        {
+            Type = type;
+            Pair = pair;
+            Data = data;
+        }
+    }
+}
