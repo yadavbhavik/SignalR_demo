fc2dcf7ef449069aab5a0d3d19b684d772e22ede
Sahil Maiyani <sahilmaiyani@gmail.com>
2019-08-21 17:22:12 +0530

    add group for signalR connection -By Sahil

--- SignalR.Hub/NotificationHub.cs
+++ SignalR.Hub/NotificationHub.cs
@@ -5,18 +5,36 @@ using System.Linq;
 using System.Threading.Tasks;
 using EventBusRabbitMQ;
 using EventBusRabbitMQ.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace SignalR.Hub
 {
     public class NotificationHub : Microsoft.AspNetCore.SignalR.Hub
     {
-        public NotificationHub()
+        private readonly IConfiguration configuration;
+
+        public NotificationHub(IConfiguration configuration)
         {
+            this.configuration = configuration;
         }
         //add configuration by bhavik yadav date:09/08/19
         public override async Task OnConnectedAsync()
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, "test_notification");
+
+            //espay-layout-beta project signalR client connection group -Sahil 21-08-2019
+
+            //get default pair. when client connection created all client group into Default pair defined in appsettings -Sahil 21-08-2019
+            string Pair = configuration.GetValue<string>("SignalRKey:DefaultPair");
+
+            Groups.AddToGroupAsync(Context.ConnectionId, "BuyerBook:" + Pair).Wait();
+            Groups.AddToGroupAsync(Context.ConnectionId, "SellerBook:" + Pair).Wait();
+
+
+            //espay-layout-beta Arbitrage Connection -Sahil 21-08-2019
+            string BaseCurrencyArbitrage = configuration.GetValue<string>("SignalRKey:DefaultBaseCurrencyArbitrage");
+            Groups.AddToGroupAsync(Context.ConnectionId, "PairDataArbitrage:" + BaseCurrencyArbitrage).Wait();
+
             await base.OnConnectedAsync();
         }
 
