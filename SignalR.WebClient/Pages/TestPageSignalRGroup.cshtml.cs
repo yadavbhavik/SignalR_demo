4ec40f0d6eb14403f09e51f99d0855f9e55664df
Sahil Maiyani <sahilmaiyani@gmail.com>
2019-08-29 16:59:53 +0530

    testing beta project signalr group wise testing implementation

---
+++ SignalR.WebClient/Pages/TestPageSignalRGroup.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SignalR.WebClient.Pages
+{
+    public class TestPageSignalRGroupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
